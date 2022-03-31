3675c7a162687955def809a60cc51385fd72bd4d
vikramkhaire <vickykhaire@gmail.com>
2022-03-31 23:53:58 +0530

    trying

--- cloudy/bla_pg0003_aiswrya.py
+++ cloudy/bla_pg0003_aiswrya.py
@@ -13,6 +13,8 @@ Si+2	12.94	0.1
 O+5	    13.79	0.04
 """
 
+# for all ions
+"""
 ions_to_use = ['C+', 'C+2', 'Si+', 'Si+2', 'O+5']
 data_col_log = [13.41, 13.62, 12.46, 12.94, 13.79]
 sigma_col_log = [0.03, 0.09, 0.05, 0.10, 0.04]
@@ -20,6 +22,25 @@ sigma_col_log = [0.03, 0.09, 0.05, 0.10, 0.04]
 Q_uvb= '18'
 uvb = 'KS18'
 figname = 'all_ions_full'
+
+interp_logf = interpol(model_path = model_path, ions_to_use= ions_to_use, Q_uvb= Q_uvb, uvb=uvb)
+
+flat_samples, ndim  = run_mcmc(data_col = data_col_log, sigma_col =sigma_col_log, interp_logf = interp_logf, figname = figname + '.pdf')
+
+# file to save mcmc chain
+save_file_name = figname
+np.save(save_file_name, flat_samples)
+
+"""
+
+# removing O+5
+ions_to_use = ['C+', 'C+2', 'Si+', 'Si+2']
+data_col_log = [13.41, 13.62, 12.46, 12.94]
+sigma_col_log = [0.03, 0.09, 0.05, 0.10]
+
+Q_uvb= '18'
+uvb = 'KS18'
+figname = 'without_o6_highT'
 
 interp_logf = interpol(model_path = model_path, ions_to_use= ions_to_use, Q_uvb= Q_uvb, uvb=uvb)
 
