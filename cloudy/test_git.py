1ab8c5811070ea1a3d5ac1eb728b7af8a0a1976c
vikramkhaire <vickykhaire@gmail.com>
2023-06-01 15:22:56 +0530

    test for abraham

---
+++ cloudy/test_git.py
@@ -0,0 +1,8 @@
+# A simple code to run cloudy and store its output
+
+import os
+import numpy as np
+import astropy.table as tab
+import subprocess
+from write_uvb_in_cloudy_format import write_uvb_in_cloudy_format
+
